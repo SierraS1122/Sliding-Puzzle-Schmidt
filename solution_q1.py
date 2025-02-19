226f7eb4081d6b854a27442d9c87edc65ea2897a
SierraS1122 <sierras1122@gmail.com>
2025-02-18 21:15:41 -0500

    Progress on all

--- solution_q1.py
+++ solution_q1.py
@@ -1,13 +1,13 @@
-
 #Geez I haven't had to code in ages
 
+goal_state = ['_','1','2','3','4','5','6','7','8']
 
 def representation(lst):
     print(lst[0],lst[1],lst[2])
     print(lst[3],lst[4],lst[5])
     print(lst[6],lst[7],lst[8])
 
-file = open("input.txt",r"r")
+file = open("input.txt","r")
 input_str = file.read()
 #print(input_str)
 file.close()
@@ -19,227 +19,357 @@ input_lst = input_str.split(',')
 #print(type(input_lst))
 
 cur_lst = input_lst
+cur_action = []
+
+state_queue = []
+state_queue += [cur_lst]
+
+action_queue = [[]]
+action_queue += cur_action
+states_visited = []
+
+states_visited += [cur_lst]
 
 print("Starting Puzzle")
 representation(cur_lst)
 
-#Top Left Empty Moves
-if cur_lst[0] == "_":
-    
-    #Option 1 (Number Right)
-        opt1_lst = cur_lst.copy()
-        opt1_lst[0] = cur_lst[1]
-        opt1_lst[1] = cur_lst[0]
-        print('')
-        print("Move:", opt1_lst[0],'L', sep='')
-        representation(opt1_lst)
-
-    #Option 2 (Number Below)
-        opt2_lst = cur_lst.copy()
-        opt2_lst[0] = cur_lst[3]
-        opt2_lst[3] = cur_lst[0]
-        print('')
-        print("Move:", opt2_lst[0],'U', sep='')
-        representation(opt2_lst)
+def generate_next_states(cur_lst, state_queue, cur_action, action_queue, states_visited):
+    #Top Left Empty Moves
+    if cur_lst[0] == "_":
         
+        #Option 1 (Number Right)
+            opt1_lst = cur_lst.copy()
+            opt1_lst[0] = cur_lst[1]
+            opt1_lst[1] = cur_lst[0]
+            print('')
+            move = opt1_lst[0]+'L'
+            print("Move:", move) 
+            representation(opt1_lst)
+            if opt1_lst == goal_state:
+                 print("WE DID IT!")
+            if opt1_lst not in states_visited:
+                state_queue += [opt1_lst]
+                states_visited += [opt1_lst]
+                action_queue += [cur_action,move]
+                print (action_queue)
+
+        #Option 2 (Number Below)
+            opt2_lst = cur_lst.copy()
+            opt2_lst[0] = cur_lst[3]
+            opt2_lst[3] = cur_lst[0]
+            print('')
+            move = opt2_lst[0]+'U'
+            print("Move:", move)
+            representation(opt2_lst)
+            if opt2_lst not in states_visited:
+                state_queue += [opt2_lst]
+                states_visited += [opt2_lst]
+                action_queue += [cur_action,move]
+                print (action_queue)
+        
+
+
+
+    #Top Middle Empty Moves
+    if cur_lst[1] == "_":
+        #Option 1 (Number Left)
+            opt1_lst = cur_lst.copy()
+            opt1_lst[1] = cur_lst[0]
+            opt1_lst[0] = cur_lst[1]
+            print('')
+            move = opt1_lst[1]+'R'
+            print("Move:", move)
+            representation(opt1_lst)
+            if opt1_lst == goal_state:
+                 print("WE DID IT!")
+            if opt1_lst not in states_visited:
+                state_queue += [opt1_lst]
+                states_visited += [opt1_lst]
+                action_queue += [[cur_action,move]]
+                print (action_queue)
+
+        #Option 2 (Number Right)
+            opt2_lst = cur_lst.copy()
+            opt2_lst[1] = cur_lst[2]
+            opt2_lst[2] = cur_lst[1]
+            print('')
+            move = opt2_lst[1]+'L'
+            print("Move:", move)
+            representation(opt2_lst)
+            if opt2_lst not in states_visited:
+                state_queue += [opt2_lst]
+                states_visited += [opt2_lst]
+                
+        #Option 3 (Number Below)
+            opt3_lst = cur_lst.copy()
+            opt3_lst[1] = cur_lst[4]
+            opt3_lst[4] = cur_lst[1]
+            print('')
+            move = opt3_lst[1]+'U'
+            print("Move:", move)
+            representation(opt3_lst)
+            print("Current State", opt3_lst)
+            print("States visisted", states_visited)
+
+            if opt3_lst not in states_visited:
+                state_queue += [opt3_lst]
+                states_visited += [opt3_lst]
+                
+                
+    #Top Right Empty Moves
+    if cur_lst[2] == "_":
+        
+        #Option 1 (Number Left)
+            opt1_lst = cur_lst.copy()
+            opt1_lst[2] = cur_lst[1]
+            opt1_lst[1] = cur_lst[2]
+            print('')
+            move = opt1_lst[2]+'R'
+            print("Move:", move)
+            representation(opt1_lst)
+            if opt1_lst not in states_visited:
+                state_queue += [opt1_lst]
+                states_visited += [opt1_lst]
+
+        #Option 2 (Number Below)
+            opt2_lst = cur_lst.copy()
+            opt2_lst[2] = cur_lst[5]
+            opt2_lst[5] = cur_lst[2]
+            print('')
+            move = opt2_lst[2]+'U'
+            print("Move:", move)
+            representation(opt2_lst)
+            if opt2_lst not in states_visited:
+                state_queue += [opt2_lst]
+                states_visited += [opt2_lst]
     
 
+    #Middle Left Empty Moves
+    if cur_lst[3] == "_":
+        #Option 1 (Number Up)
+            opt1_lst = cur_lst.copy()
+            opt1_lst[3] = cur_lst[0]
+            opt1_lst[0] = cur_lst[3]
+            print('')
+            move = opt1_lst[3]+'D'
+            print("Move:", move)
+            representation(opt1_lst)
+            if opt1_lst not in states_visited:
+                state_queue += [opt1_lst]
+                states_visited += [opt1_lst]
+
+        #Option 2 (Number Right)
+            opt2_lst = cur_lst.copy()
+            opt2_lst[3] = cur_lst[4]
+            opt2_lst[4] = cur_lst[3]
+            print('')
+            move = opt2_lst[3]+'L'
+            print("Move:", move)
+            representation(opt2_lst)
+            if opt2_lst not in states_visited:
+                state_queue += [opt2_lst]
+                states_visited += [opt2_lst]
+                
+        #Option 3 (Number Below)
+            opt3_lst = cur_lst.copy()
+            opt3_lst[3] = cur_lst[6]
+            opt3_lst[6] = cur_lst[3]
+            print('')
+            move = opt3_lst[3]+'U'
+            print("Move:", move)
+            representation(opt3_lst)
+            if opt3_lst not in states_visited:
+                state_queue += [opt3_lst]
+                states_visited += [opt3_lst]
+
+    #Middle Middle Empty Moves
+    if cur_lst[4] == "_":
+        #Option 1 (Number Up)
+            opt1_lst = cur_lst.copy()
+            opt1_lst[4] = cur_lst[1]
+            opt1_lst[1] = cur_lst[4]
+            print('')
+            move = opt1_lst[4]+'D'
+            print("Move:",move)
+            representation(opt1_lst)
+            if opt1_lst not in states_visited:
+                state_queue += [opt1_lst]
+                states_visited += [opt1_lst]
+                action_queue += [[cur_action,move]]
+                print (action_queue)
+
+        #Option 2 (Number Right)
+            opt2_lst = cur_lst.copy()
+            opt2_lst[4] = cur_lst[5]
+            opt2_lst[5] = cur_lst[4]
+            print('')
+            move =  opt2_lst[4]+'L'
+            print("Move:", move)
+            representation(opt2_lst)
+            if opt2_lst not in states_visited:
+                state_queue += [opt2_lst]
+                states_visited += [opt2_lst]
+                action_queue += [[cur_action,move]]
+                print (action_queue)
+                
+        #Option 3 (Number Below)
+            opt3_lst = cur_lst.copy()
+            opt3_lst[4] = cur_lst[7]
+            opt3_lst[7] = cur_lst[4]
+            print('')
+            move = opt3_lst[4]+'U'
+            print("Move:", move)
+            representation(opt3_lst)
+            if opt3_lst not in states_visited:
+                state_queue += [opt3_lst]
+                states_visited += [opt3_lst]
+                action_queue += [cur_action,move]
+                print (action_queue)
+
+        #Option 4 (Number Left)
+            opt4_lst = cur_lst.copy()
+            opt4_lst[4] = cur_lst[3]
+            opt4_lst[3] = cur_lst[4]
+            print('')
+            move = opt4_lst[4]+'R'
+            print("Move:", move)
+            representation(opt4_lst)
+            if opt4_lst not in states_visited:
+                state_queue += [opt4_lst]
+                states_visited += [opt4_lst]
+                action_queue += [cur_action,move]
+                print (action_queue)
+
+    #Middle Right Empty Moves
+    if cur_lst[5] == "_":
+        #Option 1 (Number Up)
+            opt1_lst = cur_lst.copy()
+            opt1_lst[5] = cur_lst[2]
+            opt1_lst[2] = cur_lst[5]
+            print('')
+            print("Move:", opt1_lst[5]+'D')
+            representation(opt1_lst)
+            if opt1_lst not in states_visited:
+                state_queue += [opt1_lst]
+                states_visited += [opt1_lst]
+
+        #Option 2 (Number Left)
+            opt2_lst = cur_lst.copy()
+            opt2_lst[5] = cur_lst[4]
+            opt2_lst[4] = cur_lst[5]
+            print('')
+            print("Move:", opt2_lst[5]+'R')
+            representation(opt2_lst)
+            if opt2_lst not in states_visited:
+                state_queue += [opt2_lst]
+                states_visited += [opt2_lst]
+                
+        #Option 3 (Number Below)
+            opt3_lst = cur_lst.copy()
+            opt3_lst[5] = cur_lst[8]
+            opt3_lst[8] = cur_lst[5]
+            print('')
+            print("Move:", opt3_lst[5]+'U')
+            representation(opt3_lst)
+            if opt3_lst not in states_visited:
+                state_queue += [opt3_lst]
+                states_visited += [opt3_lst]
+
+    #Bottom Left Empty Moves
+    if cur_lst[6] == "_":
+        #Option 1 (Number Up)
+            opt1_lst = cur_lst.copy()
+            opt1_lst[6] = cur_lst[3]
+            opt1_lst[3] = cur_lst[6]
+            print('')
+            print("Move:", opt1_lst[6]+'D')
+            representation(opt1_lst)
+            if opt1_lst not in states_visited:
+                state_queue += [opt1_lst]
+                states_visited += [opt1_lst]
+
+        #Option 2 (Number Right)
+            opt2_lst = cur_lst.copy()
+            opt2_lst[6] = cur_lst[7]
+            opt2_lst[7] = cur_lst[6]
+            print('')
+            print("Move:", opt2_lst[6]+'L')
+            representation(opt2_lst)
+            if opt2_lst not in states_visited:
+                state_queue += [opt2_lst]
+                states_visited += [opt2_lst]
+                
+
+    #Bottom Middle Empty Moves
+    if cur_lst[7] == "_":
+        #Option 1 (Number Up)
+            opt1_lst = cur_lst.copy()
+            opt1_lst[7] = cur_lst[4]
+            opt1_lst[4] = cur_lst[7]
+            print('')
+            print("Move:", opt1_lst[7]+'D')
+            representation(opt1_lst)
+            if opt1_lst not in states_visited:
+                state_queue += [opt1_lst]
+                states_visited += [opt1_lst]
+
+        #Option 2 (Number Right)
+            opt2_lst = cur_lst.copy()
+            opt2_lst[7] = cur_lst[8]
+            opt2_lst[8] = cur_lst[7]
+            print('')
+            print("Move:", opt2_lst[7]+'L')
+            representation(opt2_lst)
+            if opt2_lst not in states_visited:
+                state_queue += [opt2_lst]
+                states_visited += [opt2_lst]
+
+        #Option 3 (Number Left)
+            opt3_lst = cur_lst.copy()
+            opt3_lst[7] = cur_lst[6]
+            opt3_lst[6] = cur_lst[7]
+            print('')
+            print("Move:", opt3_lst[7]+'R')
+            representation(opt3_lst)
+            if opt3_lst not in states_visited:
+                state_queue += [opt3_lst]
+                states_visited += [opt3_lst]
+                
+    #Bottom Right Empty Moves
+    if cur_lst[8] == "_":
+        #Option 1 (Number Up)
+            opt1_lst = cur_lst.copy()
+            opt1_lst[8] = cur_lst[5]
+            opt1_lst[5] = cur_lst[8]
+            print('')
+            print("Move:", opt1_lst[8]+'D')
+            representation(opt1_lst)
+            if opt1_lst not in states_visited:
+                state_queue += [opt1_lst]
+                states_visited += [opt1_lst]
+
+        #Option 2 (Number Left)
+            opt2_lst = cur_lst.copy()
+            opt2_lst[8] = cur_lst[7]
+            opt2_lst[7] = cur_lst[8]
+            print('')
+            move = opt2_lst[8]+'R'
+            print("Move:", move)
+            representation(opt2_lst)
+            if opt2_lst not in states_visited:
+                state_queue += [opt2_lst]
+                states_visited += [opt2_lst]
+
+while cur_lst != ['_','1','2','3','4','5','6','7','8']:
+    generate_next_states(cur_lst, state_queue, cur_action, action_queue,states_visited)
+    print(state_queue)
+    cur_lst = state_queue[1]
+    cur_action = action_queue[1]
+    del(action_queue[0])
+    del(state_queue[0])
+    print("State Queue after", state_queue)
+    print("Action Queue after", action_queue)
 
 
-#Top Middle Empty Moves
-if cur_lst[1] == "_":
-    #Option 1 (Number Left)
-        opt1_lst = cur_lst.copy()
-        opt1_lst[1] = cur_lst[0]
-        opt1_lst[0] = cur_lst[1]
-        print('')
-        print("Move:", opt1_lst[1],'R', sep='')
-        representation(opt1_lst)
 
-    #Option 2 (Number Right)
-        opt2_lst = cur_lst.copy()
-        opt2_lst[1] = cur_lst[2]
-        opt2_lst[2] = cur_lst[1]
-        print('')
-        print("Move:", opt2_lst[1],'L', sep='')
-        representation(opt2_lst)
-              
-    #Option 3 (Number Below)
-        opt3_lst = cur_lst.copy()
-        opt3_lst[1] = cur_lst[4]
-        opt3_lst[4] = cur_lst[1]
-        print('')
-        print("Move:", opt3_lst[1],'U', sep='')
-        representation(opt3_lst)
-              
-#Top Right Empty Moves
-if cur_lst[2] == "_":
-    
-    #Option 1 (Number Left)
-        opt1_lst = cur_lst.copy()
-        opt1_lst[2] = cur_lst[1]
-        opt1_lst[1] = cur_lst[2]
-        print('')
-        print("Move:", opt1_lst[2],'R', sep='')
-        representation(opt1_lst)
-
-    #Option 2 (Number Below)
-        opt2_lst = cur_lst.copy()
-        opt2_lst[2] = cur_lst[5]
-        opt2_lst[5] = cur_lst[2]
-        print('')
-        print("Move:", opt2_lst[2],'U', sep='')
-        representation(opt2_lst)
-   
-
-#Middle Left Empty Moves
-if cur_lst[3] == "_":
-    #Option 1 (Number Up)
-        opt1_lst = cur_lst.copy()
-        opt1_lst[3] = cur_lst[0]
-        opt1_lst[0] = cur_lst[3]
-        print('')
-        print("Move:", opt1_lst[3],'D', sep='')
-        representation(opt1_lst)
-
-    #Option 2 (Number Right)
-        opt2_lst = cur_lst.copy()
-        opt2_lst[3] = cur_lst[4]
-        opt2_lst[4] = cur_lst[3]
-        print('')
-        print("Move:", opt2_lst[3],'L', sep='')
-        representation(opt2_lst)
-              
-    #Option 3 (Number Below)
-        opt3_lst = cur_lst.copy()
-        opt3_lst[3] = cur_lst[6]
-        opt3_lst[6] = cur_lst[3]
-        print('')
-        print("Move:", opt3_lst[3],'U', sep='')
-        representation(opt3_lst)
-
-
-
-#Middle Middle Empty Moves
-if cur_lst[4] == "_":
-    #Option 1 (Number Up)
-        opt1_lst = cur_lst.copy()
-        opt1_lst[4] = cur_lst[1]
-        opt1_lst[1] = cur_lst[4]
-        print('')
-        print("Move:", opt1_lst[4],'D', sep='')
-        representation(opt1_lst)
-
-    #Option 2 (Number Right)
-        opt2_lst = cur_lst.copy()
-        opt2_lst[4] = cur_lst[5]
-        opt2_lst[5] = cur_lst[4]
-        print('')
-        print("Move:", opt2_lst[4],'L', sep='')
-        representation(opt2_lst)
-              
-    #Option 3 (Number Below)
-        opt3_lst = cur_lst.copy()
-        opt3_lst[4] = cur_lst[7]
-        opt3_lst[7] = cur_lst[4]
-        print('')
-        print("Move:", opt3_lst[4],'U', sep='')
-        representation(opt3_lst)
-
-    #Option 4 (Number Left)
-        opt4_lst = cur_lst.copy()
-        opt4_lst[4] = cur_lst[3]
-        opt4_lst[3] = cur_lst[4]
-        print('')
-        print("Move:", opt4_lst[4],'R', sep='')
-        representation(opt4_lst)
-
-#Middle Right Empty Moves
-if cur_lst[5] == "_":
-    #Option 1 (Number Up)
-        opt1_lst = cur_lst.copy()
-        opt1_lst[5] = cur_lst[2]
-        opt1_lst[2] = cur_lst[5]
-        print('')
-        print("Move:", opt1_lst[5],'D', sep='')
-        representation(opt1_lst)
-
-    #Option 2 (Number Left)
-        opt2_lst = cur_lst.copy()
-        opt2_lst[5] = cur_lst[4]
-        opt2_lst[4] = cur_lst[5]
-        print('')
-        print("Move:", opt2_lst[5],'R', sep='')
-        representation(opt2_lst)
-              
-    #Option 3 (Number Below)
-        opt3_lst = cur_lst.copy()
-        opt3_lst[5] = cur_lst[8]
-        opt3_lst[8] = cur_lst[5]
-        print('')
-        print("Move:", opt3_lst[5],'U', sep='')
-        representation(opt3_lst)
-
-#Bottom Left Empty Moves
-if cur_lst[6] == "_":
-    #Option 1 (Number Up)
-        opt1_lst = cur_lst.copy()
-        opt1_lst[6] = cur_lst[3]
-        opt1_lst[3] = cur_lst[6]
-        print('')
-        print("Move:", opt1_lst[6],'D', sep='')
-        representation(opt1_lst)
-
-    #Option 2 (Number Right)
-        opt2_lst = cur_lst.copy()
-        opt2_lst[6] = cur_lst[7]
-        opt2_lst[7] = cur_lst[6]
-        print('')
-        print("Move:", opt2_lst[6],'L', sep='')
-        representation(opt2_lst)
-              
-
-#Bottom Middle Empty Moves
-if cur_lst[7] == "_":
-    #Option 1 (Number Up)
-        opt1_lst = cur_lst.copy()
-        opt1_lst[7] = cur_lst[4]
-        opt1_lst[4] = cur_lst[7]
-        print('')
-        print("Move:", opt1_lst[7],'D', sep='')
-        representation(opt1_lst)
-
-    #Option 2 (Number Right)
-        opt2_lst = cur_lst.copy()
-        opt2_lst[7] = cur_lst[8]
-        opt2_lst[8] = cur_lst[7]
-        print('')
-        print("Move:", opt2_lst[7],'L', sep='')
-        representation(opt2_lst)
-
-    #Option 3 (Number Left)
-        opt3_lst = cur_lst.copy()
-        opt3_lst[7] = cur_lst[6]
-        opt3_lst[6] = cur_lst[7]
-        print('')
-        print("Move:", opt3_lst[7],'R', sep='')
-        representation(opt3_lst)
-              
-#Bottom Right Empty Moves
-if cur_lst[8] == "_":
-    #Option 1 (Number Up)
-        opt1_lst = cur_lst.copy()
-        opt1_lst[8] = cur_lst[5]
-        opt1_lst[5] = cur_lst[8]
-        print('')
-        print("Move:", opt1_lst[8],'D', sep='')
-        representation(opt1_lst)
-
-    #Option 2 (Number Left)
-        opt2_lst = cur_lst.copy()
-        opt2_lst[8] = cur_lst[7]
-        opt2_lst[7] = cur_lst[8]
-        print('')
-        print("Move:", opt2_lst[8],'R', sep='')
-        representation(opt2_lst)
-
+print("Found a Solution")
